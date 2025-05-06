58f4397a0f9249a51b339e58cf73dc0cd86ff741
wuyouyuan <wuyouyuan@wegooooo.com>
2025-05-06 09:38:36 +0800

    取消注释

--- test/tests/test_微商相册A类.py
+++ test/tests/test_微商相册A类.py
@@ -267,170 +267,170 @@ class TestCompanyA:
             PageChooseGoods.page_choose_payment_status()
             PageChooseGoods.page_choose_confirm_order()
 
-    # @dog.title("通用_0021:访客足迹")
-    # def test_0021(self):
-    #     with dog.step("A进入B相册主页查看"):
-    #         PageMain.tab_friends()
-    #         PageFriends.goto_other_album(album_name="test01")
-    #         PageChooseGoods.page_back_lever()
-    #         PageMain.tab_mine()
-    #         PageMine.page_mine_configure()
-    #     with dog.step("切换B账号"):
-    #         PageConfigure.page_switch_account()
-    #         PageConfigure.page_config_click()
-    #         PageMain.tab_mine()
-    #         PageMine.page_mine_visitor()
-    #         PageVisitor.check_page_visitor_title()
-    #         PageVisitor.check_page_visitor()
-    #         PageChooseGoods.page_back_lever()
-    #         PageMine.page_mine_configure()
-    #         PageConfigure.page_switch_account()
-    #         PageConfigure.page_config_other_click()
-    #
-    # @dog.title("通用_0023:收款码支付")
-    # def test_0023(self):
-    #     with dog.step("A进入工作台，保存收款码"):
-    #         PageMain.tab_workbench()
-    #         PageWorkBench.receiving_payment_code()
-    #         PageReceiveAndPayment.page_click_payment_code()
-    #         PageReceiveAndPayment.page_send_friend_window()
-    #         PageShare.choose_wechat()
-    #         PageWechat.send_to_file_assistant()
-    #         PageWechat.click_wx_payment_code()
-    #         PageWechat.page_payment_amount()
-    #
-    # @dog.title("通用_0026:加购自己商品")
-    # def test_0026(self):
-    #     with dog.step("加购自己的商品--开单"):
-    #         PageMain.tab_dynamic()
-    #         BasePageAlbumDynamic.text_search()
-    #         PageAddProductsCart.page_products_invoice()
-    #         PageAddProductsCart.check_products_invoice()
-    #
-    #     with dog.step("加购自己的商品--立即购买"):
-    #         PageAddProductsCart.page_clean_invoice()
-    #         PageAddProductsCart.page_products_buy()
-    #         PageAddProductsCart.check_enter_order()
-    #
-    # @dog.title("通用_0027_1:采购商品--在未开通在线收款")
-    # def test_0027_1(self):
-    #     with dog.step("采购未开通在线收款用户的商品"):
-    #         PageMain.tab_friends()
-    #         PageFriends.goto_other_album(album_name="test01")
-    #         PageShop.shop_cart_add()
-    #         PageShop.page_add_shop_car()
-    #         PageAddProductsCart.page_click_purchase()
-    #         PageAddProductsCart.page_click_purchase_order()
-    #         PageAddProductsCart.page_phone_remind_window()
-    #     with dog.step("确认采购页检查"):
-    #         PageAddProductsCart.check_ensure_purchase()
-    #
-    # @dog.title("通用_0027_2:采购商品--已开通在线收款")
-    # def test_0027_2(self):
-    #     with dog.step("采购已开通在线收款用户的商品"):
-    #         PageMain.tab_friends()
-    #         PageFriends.my_album()
-    #         PageShop.shop_mine_cart_add()
-    #
-    #         PageAddProductsCart.page_mine_shop_buy()
-    #         PageAddProductsCart.page_commit_order_buy()
-    #         PageShare.choose_wechat()
-    #         PageAddProductsCart.page_no_pay_windows()
-    #         PageAddProductsCart.check_purchase_pay_page()
-    #
-    #
-    #
-    # @dog.title("通用_0011:IM消息")
-    # def test_0011(self):
-    #     with dog.step("冒泡查看给test01发送消息"):
-    #         PageMain.tab_friends()
-    #         PageFriends.goto_other_album(album_name="test01")
-    #         PageShop.page_friend_contact()
-    #         PageImMessage.page_online_chat()
-    #         PageImMessage.page_message_windows()
-    #
-    #         PageImMessage.page_send_sticker()
-    #         PageImMessage.page_send_sticker_packs()
-    #         PageImMessage.page_click_send_enter()
-    #
-    #         PageImMessage.page_send_message()
-    #
-    #         PageImMessage.page_click_plus_sign()
-    #         PageImMessage.page_choose_photo_album()
-    #         PageImMessage.page_two_back()
-    #     with dog.step("切换test01账号,并查看对应消息是否正常展示"):
-    #         PageMain.tab_mine()
-    #         PageMine.page_mine_configure()
-    #         PageConfigure.page_switch_account()
-    #         PageConfigure.page_config_click()
-    #         PageMain.tab_message()
-    #         PageImMessage.page_send_message_switch()
-    #         PageImMessage.check_im_message()
-    #         PageChooseGoods.page_back_lever()
-    #     with dog.step("切换冒泡账号"):
-    #         PageMain.tab_mine()
-    #         PageMine.page_mine_configure()
-    #         PageConfigure.page_switch_account()
-    #         PageConfigure.page_config_other_click()
-    #
-    # @dog.title("通用_0029:云盘一键清理")
-    # def test_0029(self):
-    #     with dog.step("冒泡账号云盘一键清理"):
-    #         PageMain.tab_friends()
-    #         PageFriends.my_album()
-    #         PageShop.batch_edit_share()
-    #         PageShop.page_shop_clean_up()
-    #         PageCleanUpImages.page_clean_image()
-    #         PageCleanUpImages.page_check_cloud_space()
-    #         PageImMessage.page_two_back()
-    #     with dog.step("个人相册查看一个月前的商品是否删除"):
-    #         PageCleanUpImages.check_shop_cloud_commodity()
-    #         PageShop.batch_edit_share()
-    #         PageShop.page_shop_clean_up()
-    #         PageCleanUpImages.page_recycle_bin()
-    #         PageCleanUpImages.page_recycle_restore_all()
-    #         PageCleanUpImages.page_recycle_restore_listed()
-    #
-    # @dog.title("通用_0028:开播功能验证")
-    # def test_0028(self):
-    #     with dog.step("冒泡账号开启直播"):
-    #         PageMain.tab_dynamic()
-    #         BasePageAlbumDynamic.page_add_content()
-    #         PageLiveStream.page_private_domain_live()
-    #         PageLiveStream.page_live_streaming_all()
-    #         PageLiveStream.check_page_live()
-    #         PageLiveStream.page_live_continue()
-    #         PageLiveStream.page_share_live()
-    #         PageLiveStream.page_share_live_wechat()
-    #         PageShare.choose_wechat()
-    #         PageWechat.send_to_file_assistant()
-    #         PageWechat.page_wechat_live()
-    #         PageLiveStream.page_live_start_app()
-    #         PageMain.tab_mine()
-    #     with dog.step("切换test01账号看直播"):
-    #         PageMine.page_mine_configure()
-    #         PageConfigure.page_switch_account()
-    #         PageConfigure.page_config_click()
-    #         PageMain.tab_mine()
-    #         PageMine.page_switch_sub_account()
-    #         BasePageAlbumDynamic.page_add_content()
-    #         PageLiveStream.page_private_domain_live()
-    #         PageLiveStream.page_assistant_live()
-    #         PageLiveStream.check_page_live()
-    #
-    # @dog.title("通用_0024:员工账号权限验证")
-    # def test_0024(self):
-    #     with dog.step("子账号相册动态页搜索"):
-    #         BasePageAlbumDynamic.page_team_permission_search()
-    #         PageTeamPermissions.check_team_permissions_price()
-    #
-    # @dog.title("通用_0025:员工账号权限验证")
-    # def test_0025(self):
-    #     with dog.step("我的钱包"):
-    #         PageMain.tab_mine()
-    #         PageMine.page_mine_my_wallet()
-    #         PageChooseGoods.page_back_lever()
-    #         PageMain.tab_workbench()
-    #         PageWorkBench.page_work_check_earning()
-    #         PageTeamPermissions.check_team_statistical_data()
-    #
+    @dog.title("通用_0021:访客足迹")
+    def test_0021(self):
+        with dog.step("A进入B相册主页查看"):
+            PageMain.tab_friends()
+            PageFriends.goto_other_album(album_name="test01")
+            PageChooseGoods.page_back_lever()
+            PageMain.tab_mine()
+            PageMine.page_mine_configure()
+        with dog.step("切换B账号"):
+            PageConfigure.page_switch_account()
+            PageConfigure.page_config_click()
+            PageMain.tab_mine()
+            PageMine.page_mine_visitor()
+            PageVisitor.check_page_visitor_title()
+            PageVisitor.check_page_visitor()
+            PageChooseGoods.page_back_lever()
+            PageMine.page_mine_configure()
+            PageConfigure.page_switch_account()
+            PageConfigure.page_config_other_click()
+
+    @dog.title("通用_0023:收款码支付")
+    def test_0023(self):
+        with dog.step("A进入工作台，保存收款码"):
+            PageMain.tab_workbench()
+            PageWorkBench.receiving_payment_code()
+            PageReceiveAndPayment.page_click_payment_code()
+            PageReceiveAndPayment.page_send_friend_window()
+            PageShare.choose_wechat()
+            PageWechat.send_to_file_assistant()
+            PageWechat.click_wx_payment_code()
+            PageWechat.page_payment_amount()
+
+    @dog.title("通用_0026:加购自己商品")
+    def test_0026(self):
+        with dog.step("加购自己的商品--开单"):
+            PageMain.tab_dynamic()
+            BasePageAlbumDynamic.text_search()
+            PageAddProductsCart.page_products_invoice()
+            PageAddProductsCart.check_products_invoice()
+
+        with dog.step("加购自己的商品--立即购买"):
+            PageAddProductsCart.page_clean_invoice()
+            PageAddProductsCart.page_products_buy()
+            PageAddProductsCart.check_enter_order()
+
+    @dog.title("通用_0027_1:采购商品--在未开通在线收款")
+    def test_0027_1(self):
+        with dog.step("采购未开通在线收款用户的商品"):
+            PageMain.tab_friends()
+            PageFriends.goto_other_album(album_name="test01")
+            PageShop.shop_cart_add()
+            PageShop.page_add_shop_car()
+            PageAddProductsCart.page_click_purchase()
+            PageAddProductsCart.page_click_purchase_order()
+            PageAddProductsCart.page_phone_remind_window()
+        with dog.step("确认采购页检查"):
+            PageAddProductsCart.check_ensure_purchase()
+
+    @dog.title("通用_0027_2:采购商品--已开通在线收款")
+    def test_0027_2(self):
+        with dog.step("采购已开通在线收款用户的商品"):
+            PageMain.tab_friends()
+            PageFriends.my_album()
+            PageShop.shop_mine_cart_add()
+
+            PageAddProductsCart.page_mine_shop_buy()
+            PageAddProductsCart.page_commit_order_buy()
+            PageShare.choose_wechat()
+            PageAddProductsCart.page_no_pay_windows()
+            PageAddProductsCart.check_purchase_pay_page()
+
+
+
+    @dog.title("通用_0011:IM消息")
+    def test_0011(self):
+        with dog.step("冒泡查看给test01发送消息"):
+            PageMain.tab_friends()
+            PageFriends.goto_other_album(album_name="test01")
+            PageShop.page_friend_contact()
+            PageImMessage.page_online_chat()
+            PageImMessage.page_message_windows()
+
+            PageImMessage.page_send_sticker()
+            PageImMessage.page_send_sticker_packs()
+            PageImMessage.page_click_send_enter()
+
+            PageImMessage.page_send_message()
+
+            PageImMessage.page_click_plus_sign()
+            PageImMessage.page_choose_photo_album()
+            PageImMessage.page_two_back()
+        with dog.step("切换test01账号,并查看对应消息是否正常展示"):
+            PageMain.tab_mine()
+            PageMine.page_mine_configure()
+            PageConfigure.page_switch_account()
+            PageConfigure.page_config_click()
+            PageMain.tab_message()
+            PageImMessage.page_send_message_switch()
+            PageImMessage.check_im_message()
+            PageChooseGoods.page_back_lever()
+        with dog.step("切换冒泡账号"):
+            PageMain.tab_mine()
+            PageMine.page_mine_configure()
+            PageConfigure.page_switch_account()
+            PageConfigure.page_config_other_click()
+
+    @dog.title("通用_0029:云盘一键清理")
+    def test_0029(self):
+        with dog.step("冒泡账号云盘一键清理"):
+            PageMain.tab_friends()
+            PageFriends.my_album()
+            PageShop.batch_edit_share()
+            PageShop.page_shop_clean_up()
+            PageCleanUpImages.page_clean_image()
+            PageCleanUpImages.page_check_cloud_space()
+            PageImMessage.page_two_back()
+        with dog.step("个人相册查看一个月前的商品是否删除"):
+            PageCleanUpImages.check_shop_cloud_commodity()
+            PageShop.batch_edit_share()
+            PageShop.page_shop_clean_up()
+            PageCleanUpImages.page_recycle_bin()
+            PageCleanUpImages.page_recycle_restore_all()
+            PageCleanUpImages.page_recycle_restore_listed()
+
+    @dog.title("通用_0028:开播功能验证")
+    def test_0028(self):
+        with dog.step("冒泡账号开启直播"):
+            PageMain.tab_dynamic()
+            BasePageAlbumDynamic.page_add_content()
+            PageLiveStream.page_private_domain_live()
+            PageLiveStream.page_live_streaming_all()
+            PageLiveStream.check_page_live()
+            PageLiveStream.page_live_continue()
+            PageLiveStream.page_share_live()
+            PageLiveStream.page_share_live_wechat()
+            PageShare.choose_wechat()
+            PageWechat.send_to_file_assistant()
+            PageWechat.page_wechat_live()
+            PageLiveStream.page_live_start_app()
+            PageMain.tab_mine()
+        with dog.step("切换test01账号看直播"):
+            PageMine.page_mine_configure()
+            PageConfigure.page_switch_account()
+            PageConfigure.page_config_click()
+            PageMain.tab_mine()
+            PageMine.page_switch_sub_account()
+            BasePageAlbumDynamic.page_add_content()
+            PageLiveStream.page_private_domain_live()
+            PageLiveStream.page_assistant_live()
+            PageLiveStream.check_page_live()
+
+    @dog.title("通用_0024:员工账号权限验证")
+    def test_0024(self):
+        with dog.step("子账号相册动态页搜索"):
+            BasePageAlbumDynamic.page_team_permission_search()
+            PageTeamPermissions.check_team_permissions_price()
+
+    @dog.title("通用_0025:员工账号权限验证")
+    def test_0025(self):
+        with dog.step("我的钱包"):
+            PageMain.tab_mine()
+            PageMine.page_mine_my_wallet()
+            PageChooseGoods.page_back_lever()
+            PageMain.tab_workbench()
+            PageWorkBench.page_work_check_earning()
+            PageTeamPermissions.check_team_statistical_data()
+
